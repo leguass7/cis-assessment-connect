57e10f3b758f5c2000403150fb6a8059a0a8929c
moisesgui <mgoliveira.dev@gmail.com>
2024-07-02 14:33:10 -0300

    public images include

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,23 +1,35 @@
-import { Box, Container } from "@chakra-ui/react";
+import { Box, Container, Divider, Image, Text } from "@chakra-ui/react";
 import React from "react";
 import { FormLogin } from "~/components/Forms/FormLogin";
 import { PublicLayout } from "~/components/PublicLayout";
+import astronalta from "../../../public/imgs/astro-cis.png";
 
 type Props = {};
 
 const Login: React.FC<Props> = () => {
+  const logged = true;
   return (
     <PublicLayout>
-      <Container maxW="sm" mt={10}>
-        <Box
-          p={{ base: 2, md: 8 }}
-          borderWidth={{ base: 0, md: 1 }}
-          borderRadius={{ base: 0, md: "xl" }}
-          boxShadow={{ base: 0, md: "lg" }}
-          bg={{ base: "transparent", md: "white" }}
-        >
-          <FormLogin />
-        </Box>
+      <Container mt={10}>
+        {logged ? (
+          <Box width={"100%"}>
+            <Text lineHeight={1.2} textColor={"#3b3b3b"} fontSize="5xl" as="b">
+              Usuário confirmado com sucesso.
+            </Text>
+            <Box width={70} height={1.5} bg={"#fa5b52"} borderRadius="2xl"></Box>
+            <Image src={astronalta.src} alt="Logo" width={400} />
+          </Box>
+        ) : (
+          <Box
+            p={{ base: 2, md: 8 }}
+            borderWidth={{ base: 0, md: 1 }}
+            borderRadius={{ base: 0, md: "xl" }}
+            boxShadow={{ base: 0, md: "lg" }}
+            bg={{ base: "transparent", md: "white" }}
+          >
+            <FormLogin />
+          </Box>
+        )}
       </Container>
     </PublicLayout>
   );
