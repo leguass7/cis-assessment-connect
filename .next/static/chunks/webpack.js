2115967d5ae2ef6b10c325c776fbb7f029c8936f
moisesgui <mgoliveira.dev@gmail.com>
2024-07-02 11:03:43 -0300

    .next adicionada ao gitignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b999f99c2b0dedf2"; }
+/******/ 		__webpack_require__.h = function() { return "1445053295fe67e3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
