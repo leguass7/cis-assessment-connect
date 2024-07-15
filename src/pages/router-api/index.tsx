73298fc7127404a211aad970ff72e5c13d792f1c
moisesgui <mgoliveira.dev@gmail.com>
2024-07-15 17:18:01 -0300

    refactor no componente skeleton

--- src/pages/router-api/index.tsx
+++ src/pages/router-api/index.tsx
@@ -60,7 +60,7 @@ const RouterApi = () => {
   const [showSkeleton, setShowSkeleton] = useState(false);
   const [passports, setPassports] = useState<PreparedPassport[]>([]);
 
-  const buttonBg = useColorModeValue('#2b62e3', 'cisBlue');
+  const buttonBg = useColorModeValue('cisBlue', 'gray.700');
   const bgCardColor = useColorModeValue('white', 'gray.800');
   const creditBoxColor = useColorModeValue('gray.100', 'gray.900');
   const creditTextColor = useColorModeValue('gray.700', 'gray.200');
